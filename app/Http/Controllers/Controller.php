d3d823d88f8693b28768e0eb2400d6edf5fe42b6
Isep Lutpi Nur <53908272+upi20@users.noreply.github.com>
2023-07-08 19:42:58 +0700

    move boot to controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function __construct()
+    {
+        setlocale(LC_ALL, 'IND');
+        DB::statement("SET lc_time_names = 'id_ID'");
+        Paginator::useBootstrapFive();
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        setlocale(LC_ALL, 'IND');
-        DB::statement("SET lc_time_names = 'id_ID'");
-        Paginator::useBootstrapFive();
     }
 }
